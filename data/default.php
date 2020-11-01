60d2f7da4b24209fec31ec7e8d861c9c4383d9e5
alkka00 <71847524+alkka00@users.noreply.github.com>
2020-11-01 17:11:09 +0100

    ahoj

---
+++ data/default.php
@@ -0,0 +1,24 @@
+<?php
+    $title = 'Výchozí stránka s úkolem';
+    require_once 'inc/head.php';
+
+
+
+
+
+?>
+<h1>Doplňte formulář</h1>
+
+    <form action="" method="POST">
+        <P>Email <input type="email" name="email" value=""</P>
+        <P>Heslo <input type="password" name="heslo" value=""</P>
+        <p>Uložit údaje <input type="checkbox" name="povrzeni" value="">  </p>
+        <p><input type="submit" name="submit" value="Odeslat"> </p>
+    </form>
+
+
+<?php
+// Instrukce k úkolu
+// Formulář vytvoříte klasicky z HTML
+// Jeho odeslání metodou POST zachytíte jako údaje v objektu REQUEST
+// Data získaná z tohoto objektu zatím stačí pouze zalogovat pomoví Debbugeru
